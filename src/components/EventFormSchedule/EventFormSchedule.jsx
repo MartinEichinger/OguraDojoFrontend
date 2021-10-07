5c26c700e8c5fa2700aa856bfd96e4d707a3d675
EdMadd <martl.eichinger@gmail.com>
2021-10-07 20:21:31 +0200

    init

--- src/components/EventFormSchedule/EventFormSchedule.jsx
+++ src/components/EventFormSchedule/EventFormSchedule.jsx
@@ -5,12 +5,22 @@ const EventFormSchedule = ({ events, month, selectEvent, delEvent, isAuthenticat
     <React.Fragment>
       {events.map((item, i) => {
         var d = new Date(item.date);
-        return i % 2 === 0 ? (
+        var t = new Date();
+        var uptodate = t < d;
+        return isAuthenticated ? (
           <div className="block d-flex flex-row" key={i} onClick={() => selectEvent(item)}>
-            <div className="date bgRed d-flex flex-column justify-content-center align-items-center">
-              <h1>{d.getDate()}</h1>
-              <h2>{month[d.getMonth()]}</h2>
-            </div>
+            {i % 2 === 0 && (
+              <div className="date bgRed d-flex flex-column justify-content-center align-items-center">
+                <h1>{d.getDate()}</h1>
+                <h2>{month[d.getMonth()]}</h2>
+              </div>
+            )}
+            {i % 2 === 1 && (
+              <div className="date bgGreen d-flex flex-column justify-content-center align-items-center">
+                <h1>{d.getDate()}</h1>
+                <h2>{month[d.getMonth()]}</h2>
+              </div>
+            )}
             <div className="details d-flex flex-column align-items-stretch justify-content-between">
               <div className="d-flex flex-row justify-content-between align-items-center">
                 <h3 className="align-self-stretch">{item.title}</h3>
@@ -19,34 +29,50 @@ const EventFormSchedule = ({ events, month, selectEvent, delEvent, isAuthenticat
                 )}
               </div>
               <div className="d-flex flex-row justify-content-start align-items-baseline">
-                <i className="fas fa-map-marker-alt red"></i>
-                <h4 className="red">{item.location}</h4>
+                {i % 2 === 0 && <i className="fas fa-map-marker-alt red"></i>}
+                {i % 2 === 1 && <i className="fas fa-map-marker-alt green"></i>}
+                {i % 2 === 0 && <h4 className="red">{item.location}</h4>}
+                {i % 2 === 1 && <h4 className="green">{item.location}</h4>}
                 <i className="fas fa-user"></i>
                 <h4>{item.organisator}</h4>
               </div>
             </div>
           </div>
         ) : (
-          <div className="block d-flex flex-row" key={i} onClick={() => selectEvent(item)}>
-            <div className="date bgGreen d-flex flex-column justify-content-center align-items-center">
-              <h1>{d.getDate()}</h1>
-              <h2>{month[d.getMonth()]}</h2>
-            </div>
-            <div className="details d-flex flex-column align-items-stretch justify-content-between">
-              <div className="d-flex flex-row justify-content-between align-items-center">
-                <h3 className="align-self-stretch">{item.title}</h3>
-                {isAuthenticated && (
-                  <i className="far red fa-trash-alt mr-3" onClick={() => delEvent(item)}></i>
+          <React.Fragment>
+            {uptodate && (
+              <div className="block d-flex flex-row" key={i} onClick={() => selectEvent(item)}>
+                {i % 2 === 0 && (
+                  <div className="date bgRed d-flex flex-column justify-content-center align-items-center">
+                    <h1>{d.getDate()}</h1>
+                    <h2>{month[d.getMonth()]}</h2>
+                  </div>
                 )}
+                {i % 2 === 1 && (
+                  <div className="date bgGreen d-flex flex-column justify-content-center align-items-center">
+                    <h1>{d.getDate()}</h1>
+                    <h2>{month[d.getMonth()]}</h2>
+                  </div>
+                )}
+                <div className="details d-flex flex-column align-items-stretch justify-content-between">
+                  <div className="d-flex flex-row justify-content-between align-items-center">
+                    <h3 className="align-self-stretch">{item.title}</h3>
+                    {isAuthenticated && (
+                      <i className="far red fa-trash-alt mr-3" onClick={() => delEvent(item)}></i>
+                    )}
+                  </div>
+                  <div className="d-flex flex-row justify-content-start align-items-baseline">
+                    {i % 2 === 0 && <i className="fas fa-map-marker-alt red"></i>}
+                    {i % 2 === 1 && <i className="fas fa-map-marker-alt green"></i>}
+                    {i % 2 === 0 && <h4 className="red">{item.location}</h4>}
+                    {i % 2 === 1 && <h4 className="green">{item.location}</h4>}
+                    <i className="fas fa-user"></i>
+                    <h4>{item.organisator}</h4>
+                  </div>
+                </div>
               </div>
-              <div className="d-flex flex-row justify-content-start align-items-baseline">
-                <i className="fas fa-map-marker-alt green"></i>
-                <h4 className="green">{item.location}</h4>
-                <i className="fas fa-user"></i>
-                <h4>{item.organisator}</h4>
-              </div>
-            </div>
-          </div>
+            )}
+          </React.Fragment>
         );
       })}
     </React.Fragment>
