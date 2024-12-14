add484910aee1df8d4ef97136f26a09494f20030
EdMadd <martl.eichinger@gmail.com>
2024-12-14 12:32:46 +0100

    Update icons

--- src/components/CompRoundRectList/CompRoundRectList.jsx
+++ src/components/CompRoundRectList/CompRoundRectList.jsx
@@ -4,7 +4,7 @@ const CompRoundRectList = ({ list }) => {
   return (
     <React.Fragment>
       <div className="circle d-flex flex-row align-items-center justify-content-center">
-        <img src="geld_24px_outlined.svg" alt="" />
+        <TablerIconCoin stroke={'rgb(121,0,0)'} />
         <p className="red bold">{list.title}</p>
       </div>
       <div className="rect list-rows scroll_">
@@ -24,6 +24,28 @@ const CompRoundRectList = ({ list }) => {
         })}
       </div>
     </React.Fragment>
+  );
+};
+
+const TablerIconCoin = ({ stroke = 'black', fill = 'none' }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill={fill}
+      stroke={stroke}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-coin"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+      <path d="M14.8 9a2 2 0 0 0 -1.8 -1h-2a2 2 0 1 0 0 4h2a2 2 0 1 1 0 4h-2a2 2 0 0 1 -1.8 -1" />
+      <path d="M12 7v10" />
+    </svg>
   );
 };
 
--- src/components/CompRoundRectMap/CompRoundRectMap.jsx
+++ src/components/CompRoundRectMap/CompRoundRectMap.jsx
@@ -4,7 +4,7 @@ const CompRoundRectMap = ({ map }) => {
   return (
     <React.Fragment>
       <div className="circle d-flex flex-row align-items-center justify-content-center">
-        <img src="wo_24px_outlined.svg" alt="" />
+        <TablerIconMapPin stroke="rgb(121,0,0)" />
         <p className="red bold">{map.title}</p>
       </div>
       <div className="rect d-flex flex-row scroll_">
@@ -23,6 +23,27 @@ const CompRoundRectMap = ({ map }) => {
         </div>
       </div>
     </React.Fragment>
+  );
+};
+
+const TablerIconMapPin = ({ stroke = 'black', fill = 'none' }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill={fill}
+      stroke={stroke}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-map-pin"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+      <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+    </svg>
   );
 };
 
