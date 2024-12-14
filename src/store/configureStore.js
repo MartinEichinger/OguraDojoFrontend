fdacacfd48b7dd7b85823244c93418edcfad5c4f
EdMadd <martl.eichinger@gmail.com>
2024-12-14 14:50:23 +0100

    Delete unused redux stores

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,17 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import filterReducer from './filter';
-import authReducer from './auth';
-import eventsReducer from './events';
-import blogsReducer from './blogs';
 import toasty from './middleware/toast';
 import api from './middleware/api';
 
 export const store = configureStore({
   reducer: {
     filter: filterReducer,
-    auth: authReducer,
-    events: eventsReducer,
-    blogs: blogsReducer,
   },
   //middleware: [...getDefaultMiddleware(), toasty, api, configSerialize],
   middleware: (getDefaultMiddleware) =>
