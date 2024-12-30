6d2103ec3c432a6b8c4d1a27517c20b88951e647
EdMadd <martl.eichinger@gmail.com>
2024-12-30 11:05:32 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -46,11 +46,6 @@ interface IState {
 function App() {
   const debug = false;
 
-  // BREAKPOINTS
-  const breakpoints = [400, 600, 960, 1280, 1920];
-
-  const mq = breakpoints.map((bp) => `@media (max-width: ${bp}px)`);
-
   const lang = 'de-DE'; // 'en-US' 'de-DE' 'fr-FR'
   const [state, setState] = useState<IState>({
     pageTraining: 'Training',
@@ -58,27 +53,6 @@ function App() {
     pagePanziGong: 'PanziGong',
     pageQiGong: 'QiGong',
   });
-
-  const colors = {
-    bgGreen50: 'rgba(10, 121, 0, 0.5)',
-    bgGreen: 'rgba(10, 121, 0, 1)', //HSL: 115, 100, 24 //#0A7900
-    bgBlue50: 'rgba(23,85,118,0.5)',
-    bgBlue: 'rgba(23,85,118,1)',
-    bgGrey: 'rgba(232, 232, 232, 1)',
-    bgWhite50: 'rgba(255,255,255,0.5)',
-    bgWhite75: 'rgba(255,255,255,0.75)',
-    bgWhite: 'rgba(255,255,255,1)',
-    bgRed: 'rgba(121, 0, 0, 1)', //HSL: 0, 100, 24 //#790000
-    bgRedLight: 'hsl(0,100%,36%)',
-    bgRed20: 'hsl(0,100%,20%)',
-    bgRedSolid: 'hsl(0,100%,12%)',
-    bgRed50: 'rgba(121, 0, 0, 0.5)',
-    typoGrey: 'rgba(64, 64, 64, 1)',
-    typoGreen: 'rgba(10, 121, 0, 1)',
-    typoBlue: 'rgba(23,85,118,1)',
-    typoRed: 'rgba(121, 0, 0, 1)',
-    shadowGrey: 'rgba(122,122,122,1)',
-  };
 
   const contentCards = [
     {
@@ -149,7 +123,7 @@ function App() {
   return (
     <AppBody className="App d-flex flex-column">
       <ToastContainer />
-      <Navigation colors={colors} select={selectpage} mq={mq} />
+      <Navigation select={selectpage} />
       <ModalKarate page={state.pageKarate} select={selectpage} />
       <ModalPanziGong page={state.pagePanziGong} select={selectpage} />
       <ModalQiGong page={state.pageQiGong} select={selectpage} />
